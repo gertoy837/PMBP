09d0d91b87a76b34b887dee22b88bb090587eeed
gertoy837 <dikamahardika837@gmail.com>
2024-02-23 11:02:20 +0700

    Migrasi, Model, dan Controller

---
+++ app/Http/Controllers/WaliSantriController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Wali_santri;
+use Illuminate\Http\Request;
+
+class WaliSantriController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Wali_santri $wali_santri)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Wali_santri $wali_santri)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Wali_santri $wali_santri)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Wali_santri $wali_santri)
+    {
+        //
+    }
+}
---
+++ app/Models/Wali_santri.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Wali_santri extends Model
+{
+    use HasFactory;
+}

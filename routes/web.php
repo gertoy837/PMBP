37c358d31cf4f11e7982786f21b2fb88d716cc35
firdaus <riyoyojajan53@gmail.com>
2024-02-26 08:24:15 +0700

    coba

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\AboutController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,9 +15,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('dashboard/dashboard');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::get('/', [AboutController::class, 'index'])->name('about');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
